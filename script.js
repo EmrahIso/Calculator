f55229198c18cbccdba61629ca4081d12ad01979
Emrah Isovic <emrahisovic20@gmail.com>
2024-04-21 19:10:31 +0200

    Three variables for arguments and operate function that run operations

--- script.js
+++ script.js
@@ -13,3 +13,30 @@ function multiply(numA, numB) {
 function divide(numA, numB) {
     return numA / numB;
 }
+
+//////////////////////////////////////
+
+let numberA = 4;
+let numberB = 2;
+let operationOperator = '/';
+
+//////////////////////////////////////
+
+function operate(numA, numB, operator) {
+    switch(operator) {
+        case '+':
+            return add(numA, numB)
+        break;
+        case '-':
+            return subtract(numA, numB)
+        break;
+        case '*':
+            return multiply(numA, numB)
+        break;
+        case '/':
+            return divide(numA, numB)
+        break;
+    }
+}
+
+console.log(operate(numberA, numberB, operationOperator))
