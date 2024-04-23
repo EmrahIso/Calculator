2a52ffb305d372f5e34926155482abdc0941d348
Emrah Isovic <emrahisovic20@gmail.com>
2024-04-23 20:42:32 +0200

    Added main functionality of calculator (operationStart) function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const myRe = /d(b+)d/g;
+
 function add(numA, numB) {
     return numA + numB;
 }
@@ -43,31 +45,123 @@ function operate(numA, numB, operator) {
 
 //////////////////////////////////////////////////////////
 
+let operatorArr = [
+    // add
+    {value: 0},
+
+    // subtract
+    {value: 0},
+
+    // multiply
+    {value: 0},
+
+    // divide
+    {value: 0}
+];
+
+
+
 const displayEl = document.querySelector('.display');
 const keypadEl = document.querySelector('.keypad');
 
-let displayValue = Number(displayEl.innerText);
 
+let lastClickedKey = null;
 function populateDisplay(str) {
     if(displayEl.innerText.split('').length >= 13) {
         return
-    }  else if(displayEl.innerText == '0') {
+    } else if(str == '.' && displayEl.innerText == '0') {
+        return
+    } else if(displayEl.innerText != '.' && str == '.' && lastClickedKey.search("[0-9]") != 0) {
+        return
+    }  else if(displayEl.innerText == '0' || displayEl.innerText == numberA || displayEl.innerText == numberB) {
         displayEl.innerText = str;
     } else if(str === '.' && displayEl.innerText.split('').includes('.')) {
         return
     } else {
         displayEl.innerText += str;
     }
-    displayValue = Number(displayEl.innerText);
+}
+
+// Operation Function
+
+function operationStart(targetValue) {
+    if(displayEl.innerText === '0' || displayEl.innerText.split('').length <= 0) {} else {
+        if(lastClickedKey == '+' || lastClickedKey == '-' || lastClickedKey == 'x' || lastClickedKey == '/') {
+            switch(targetValue) {
+                case '+':
+                    operationOperator = '+';
+                break;
+                case '-':
+                    operationOperator = '-';
+                break;
+                case 'x':
+                    operationOperator = '*';
+                break;
+                case '/':
+                    operationOperator = '/';
+                break;
+            }
+            return
+        }
+        switch(targetValue) {
+            case '+':
+                operatorArr[0].value++;
+            break;
+            case '-':
+                operatorArr[1].value++;
+            break;
+            case 'x':
+                operatorArr[2].value++;
+            break;
+            case '/':
+                operatorArr[3].value++;
+            break;
+        }
+
+        let sumOfOperators = operatorArr.reduce((sum, currentItem) =>  sum + currentItem.value, 0);
+        if(sumOfOperators == 1) {
+            numberA = Number(displayEl.innerText);
+        } else {
+            numberB = Number(displayEl.innerText);
+            let result = operate(numberA, numberB, operationOperator);
+            if(result.toString().length > 14) {
+                result = Number(result.toString().split('').toSpliced(12).join(''));
+            }
+            displayEl.innerText = result;
+            numberA = Number(displayEl.innerText);
+        }
+
+        switch(targetValue) {
+            case '+':
+                operationOperator = '+';
+            break;
+            case '-':
+                operationOperator = '-';
+            break;
+            case 'x':
+                operationOperator = '*';
+            break;
+            case '/':
+                operationOperator = '/';
+            break;
+        }
+    }
 }
 
 keypadEl.addEventListener('click', e => {
-    let eventTarget = e.target.className;
+    let eventTargetClass = e.target.className;
     let eventTargetValue = e.target.innerText;
-    if(eventTarget !== "keypad" && eventTarget !== 'dotBtn') { 
+    if(eventTargetClass === 'operate') {
+        console.log('operate');
+    } else if(eventTargetClass === 'clear') {
+        console.log('clear')
+    } else if(eventTargetClass === 'delete') {
+        console.log('delete')
+    } else if(eventTargetClass === 'keypad') {} else if(eventTargetClass.includes('digit') || eventTargetClass.includes('dot')) {
         populateDisplay(eventTargetValue);
-    } else if(eventTarget === "dotBtn") {
-        populateDisplay(eventTargetValue);
+    } else {
+        operationStart(eventTargetValue);
     }
+    lastClickedKey = eventTargetValue;
 })
-
+////////////////////////////////////////////////////////////////
