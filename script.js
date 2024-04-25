7eaf826dd20d91cc5184f118a85e22336a00be3b
Emrah Isovic <emrahisovic20@gmail.com>
2024-04-25 22:17:25 +0200

    Added deleteFunction() and changed architecture of whole document

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+// imported regex
 const myRe = /d(b+)d/g;
+
+//////////////////////////////////////
+// MAIN OPERATIONS
 
 function add(numA, numB) {
     return numA + numB;
@@ -17,12 +21,14 @@ function divide(numA, numB) {
 }
 
 //////////////////////////////////////
+// THREE MAIN VARIABLES
 
 let numberA = 0;
 let numberB = 0;
 let operationOperator = '';
 
 //////////////////////////////////////
+// OPERATE FUNCTION
 
 function operate(numA, numB, operator) {
     switch(operator) {
@@ -41,9 +47,9 @@ function operate(numA, numB, operator) {
     }
 }
 
-//console.log(operate(numberA, numberB, operationOperator))
 
 //////////////////////////////////////////////////////////
+// OPERATORS ARR  (counts how many times the operators clicked)
 
 let operatorArr = [
     // add
@@ -60,29 +66,42 @@ let operatorArr = [
 ];
 
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+// POPULATE FUNCTION
 
 const displayEl = document.querySelector('.display');
 const keypadEl = document.querySelector('.keypad');
 
-
 let lastClickedKey = null;
 function populateDisplay(str) {
-    if(displayEl.innerText.split('').length >= 13) {
+    if(displayEl.innerText.split('').length >= 11) {
         return
-    } else if(str == '.' && displayEl.innerText == '0') {
+    } else if (str == '.'  && (lastClickedKey == '.' || lastClickedKey == '+' || lastClickedKey == '-' || lastClickedKey == 'x' || lastClickedKey == '/')) {
+        displayEl.innerText = '0.';
+    } else if(str == '.' && lastClickedKey == 'DEL') {
+        displayEl.innerText = '0.';
+    } else if(lastClickedKey != null && displayEl.innerText != '.' && str == '.' && lastClickedKey.search("[0-9]") != 0) {
         return
-    } else if(displayEl.innerText != '.' && str == '.' && lastClickedKey.search("[0-9]") != 0) {
-        return
-    }  else if(displayEl.innerText == '0' || displayEl.innerText == numberA || displayEl.innerText == numberB) {
+    } else if(displayEl.innerText == '0') {
         displayEl.innerText = str;
-    } else if(str === '.' && displayEl.innerText.split('').includes('.')) {
-        return
-    } else {
+    } else if(lastClickedKey == '+' || lastClickedKey == '-' || lastClickedKey == 'x' || lastClickedKey == '/') {
+        displayEl.innerText = str;
+    } else if (displayEl.innerText == numberA || displayEl.innerText == numberB) {
         displayEl.innerText += str;
+    } else if(str == '.' && displayEl.innerText.split('').includes('.')) {
+        return
+    }  else {
+        displayEl.innerText += str;
+    }
+    if(displayEl.innerText[0] == '.') {
+        let newValue = displayEl.innerText.split('').toSpliced(0, 0, '0').join('');
+        displayEl.innerText = newValue;
     }
 }
 
-// Operation Function
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+// OPERATION FUNCTION
 
 function operationStart(targetValue) {
     if(displayEl.innerText === '0' || displayEl.innerText.split('').length <= 0) {} else {
@@ -121,14 +140,17 @@ function operationStart(targetValue) {
         let sumOfOperators = operatorArr.reduce((sum, currentItem) =>  sum + currentItem.value, 0);
         if(sumOfOperators == 1) {
             numberA = Number(displayEl.innerText);
+            console.log(numberA);
+        } else if(lastClickedKey == 'DEL') {
+            let result = operate(numberA, numberB, operationOperator);
+            displayEl.innerText = result;
         } else {
             numberB = Number(displayEl.innerText);
+            console.log(numberB);
             let result = operate(numberA, numberB, operationOperator);
-            if(result.toString().length > 14) {
-                result = Number(result.toString().split('').toSpliced(12).join(''));
-            }
             displayEl.innerText = result;
             numberA = Number(displayEl.innerText);
+            
         }
 
         switch(targetValue) {
@@ -148,19 +170,63 @@ function operationStart(targetValue) {
     }
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+// DELETE FUNCTION
+
+function deleteFunction() {
+    let newValue = displayEl.innerText.split('');
+    let sumOfOperators = operatorArr.reduce((sum, currentItem) =>  sum + currentItem.value, 0);
+    if(newValue.includes('.')) {
+        numberA = 0;
+        numberB = 0;
+        displayEl.innerText = '0';
+    } else if(sumOfOperators > 0) {
+        if(newValue[1] == '.') {
+            displayEl.innerText = '0';
+            numberB = 0;
+        } else {
+            newValue.pop();
+            displayEl.innerText = newValue.join('');
+            if(displayEl.innerText == '') {
+              displayEl.innerText = '0';
+            }
+            numberA = Number(displayEl.innerText);
+            numberB = 0;
+        }
+    } else {
+        if(newValue[1] == '.') {
+            displayEl.innerText = '0';
+        } else {
+            newValue.pop();
+            displayEl.innerText = newValue.join('');
+            if(displayEl.innerText == '') {
+                displayEl.innerText = '0';
+            }
+        }
+    }
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+// EVENT LISTENERS
+
 keypadEl.addEventListener('click', e => {
     let eventTargetClass = e.target.className;
     let eventTargetValue = e.target.innerText;
     if(eventTargetClass === 'operate') {
         console.log('operate');
     } else if(eventTargetClass === 'clear') {
-        console.log('clear')
+        console.log('clear');
     } else if(eventTargetClass === 'delete') {
-        console.log('delete')
+        deleteFunction()
     } else if(eventTargetClass === 'keypad') {} else if(eventTargetClass.includes('digit') || eventTargetClass.includes('dot')) {
         populateDisplay(eventTargetValue);
     } else {
         operationStart(eventTargetValue);
+    }
+    if(displayEl.innerText.length >= 11) {
+        let result = displayEl.innerText.split('').toSpliced(10).join('');
+        displayEl.innerText = result;
     }
     lastClickedKey = eventTargetValue;
 })
